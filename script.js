9d7eec708493d6da6298be77f49705b47f5660fa
Oscar <ovreyes@uci.edu>
2022-10-24 03:31:41 -0700

    updated-media

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+
+// Grabs the bar as an item 
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+
+
+const close_bar = document.getElementById("close");
+
+
+if (bar){
+    bar.addEventListener("click", () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close_bar){
+    close_bar.addEventListener("click", () => {
+        nav.classList.remove('active');
+    })
+}
